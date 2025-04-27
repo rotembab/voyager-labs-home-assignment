cbe7570842c3caeb07e683987d85b0d1bd05d5bb
Rotem Babani <rotembabmen@gmail.com>
2025-04-27 19:33:10 +0300

    added also for velocity

--- src/interfaces/node.interface.ts
+++ src/interfaces/node.interface.ts
@@ -1,7 +1,10 @@
 export interface INode {
   id: string;
 
-  //for D3 to add dynamically to the node
+  //for D3 to add position dynamically to the node
   x?: number;
   y?: number;
+  //for D3 to add velocity dynamically to the node
+  vx?: number;
+  vy?: number;
 }
