0f7d73aaa1f4134b11e7346982941a8b6c828f36
Rotem Babani <rotembabmen@gmail.com>
2025-04-29 09:10:46 +0300

    added types for d3

--- src/components/graph-canvas.component.tsx
+++ src/components/graph-canvas.component.tsx
@@ -1,5 +1,10 @@
 import { useMemo, useRef } from 'react';
 import { IGraphData } from '../interfaces/graph-data.interface';
+import * as d3 from 'd3';
+
+interface CustomNodeDatum extends d3.SimulationNodeDatum {
+  id: string;
+}
 
 type GraphCanvasProps = {
   graphData: IGraphData;
@@ -7,12 +12,26 @@ type GraphCanvasProps = {
   height: number;
 };
 
-const GraphCanvas = ({ width, height }: GraphCanvasProps) => {
+const GraphCanvas = ({ width, height, graphData }: GraphCanvasProps) => {
   const canvasRef = useRef<HTMLCanvasElement>(null);
   const canvasContext = useMemo(
     () => canvasRef.current?.getContext('2d'),
     [canvasRef.current]
   );
+  const color = d3.scaleOrdinal(d3.schemeCategory10);
+  //create copies
+  const links = graphData.links.map((d) => ({ ...d }));
+  const nodes = graphData.nodes.map((d) => ({ ...d }));
+  //create simulation
+  // const simulation = d3
+  //   .forceSimulation(nodes)
+  //   .force(
+  //     'link',
+  //     d3.forceLink(links).id((d) => d.id)
+  //   )
+  //   .force('charge', d3.forceManyBody())
+  //   .force('center', d3.forceCenter(width / 2, height / 2))
+  //   .on('tick', ticked);
 
   return (
     <div className='container'>
