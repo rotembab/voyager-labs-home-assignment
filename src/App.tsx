c1fb971ca1c11ef118a840c91eb2a69753d96892
Rotem Babani <rotembabmen@gmail.com>
2025-05-01 13:00:18 +0300

    definitive data

--- src/App.tsx
+++ src/App.tsx
@@ -4,28 +4,6 @@ import GraphCanvas from './components/graph-canvas.component';
 import data from './data/data.json';
 
 function App() {
-  // Generate random graph data with 100 nodes and 100 links
-  // Each node has a group between 1 and 5
-  // Each link has a random value between 1 and 10
-  // The source and target nodes are randomly selected from the 100 nodes
-
-  // const graphData = {
-  //   nodes: Array.from({ length: 100 }, (_, i) => ({
-  //     id: (i + 1).toString(),
-  //     group: Math.floor(Math.random() * 5) + 1, // group 1 to 5
-  //   })),
-  //   links: Array.from({ length: 100 }, () => {
-  //     const source = Math.floor(Math.random() * 100) + 1;
-  //     let target = Math.floor(Math.random() * 100) + 1;
-  //     while (target === source) target = Math.floor(Math.random() * 100) + 1;
-  //     return {
-  //       source: source.toString(),
-  //       target: target.toString(),
-  //       value: Math.random() * 10 + 1, // random value between 1 and 10
-  //     };
-  //   }),
-  // };
-
   return (
     <>
       <h1>Force Directed Graph Visualization</h1>
